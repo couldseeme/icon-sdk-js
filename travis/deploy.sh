a241ab400969ae1a379182b40466ef6e407dea9f
JH Kim <couldseeme@icon.foundation>
2019-04-01 16:01:07 +0900

    Update deploy files
    Update CI assets for auto-deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,2 +1,9 @@
-gren changelog --override --token="${GREN_GITHUB_TOKEN}"
-release-it
+npm --no-git-tag-version version patch
+auto-changelog --commit-limit 10 --ignore-commit-pattern '/changelog|CHANGELOG|readme|README|version|/i'
+version=$(node -p "require('./package.json').version")
+git add *
+git commit -m "Update version (v$version)"
+git push
+git tag -a v$version -m "Update version (v$version)"
+git push --tags origin master
+npm publish
